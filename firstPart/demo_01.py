015d3b0086fdebc711caae021ac9a675de668051
Joe-DoubleV <53033971+Joe-DoubleV@users.noreply.github.com>
2019-12-12 14:22:42 +0800

    实现温度转换

--- firstPart/demo_01.py
+++ firstPart/demo_01.py
@@ -1,5 +1,4 @@
 TempStr = input("请输入带有符号的温度：")
-
 if TempStr[-1] in ["F","f"]:
 	C = (eval(TempStr[0:-1]) - 32) / 1.8
 	print ("转换的温度是：{:.2f}C".format(C))
@@ -8,6 +7,3 @@ elif TempStr[-1] in ["C","c"]:
 	print ("转换的温度是：{:.2f}F".format(C))
 else:
 	print("输入格式错误")
-
-for index in range(1, 3):
-	print (index)
